6dd8d6e503dc80d4a0dbca7bb3d9ecacea5c20c0
Indhumathi1626 <indhumathi@baavatech.com>
2026-03-03 19:35:00 +0200

    Update p11.py
    Basic_program_2

--- p11.py
+++ p11.py
@@ -1,4 +1,5 @@
-# compare two list anf find the common elements
+# Basic program for list
+#compare two list anf find the common elements
 a = [1,2,4,5,3,6,21,34]
 b = [1,4,3,6,21]
 c = []
@@ -6,5 +7,6 @@ for x in a:
     if x in b:
         c.append(x)       
 print(c)        
+
 
    
